e11be0f23c749e6e93a50b13debb591b4056167b
Elizabeth Lizcano <lizcano.liz14@gmail.com>
2025-12-11 00:22:40 -0300

    Remove legacy Registrar API for Flutter 3.30+

--- android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
+++ android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
@@ -500,12 +500,7 @@ class HealthPlugin(private var channel: MethodChannel? = null) :
         // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
         // plugin registration via this function while apps migrate to use the new Android APIs
         // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-        //
-        // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-        // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be
-        // called
-        // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-        // in the same class.
+        
 
         override fun success(p0: Any?) {
                 handler?.post { mResult?.success(p0) }
